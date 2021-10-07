46d886464f9efbe2f442bc84a2f9786605f264c2
Karel Hala <khala@redhat.com>
2021-10-07 08:12:58 +0200

    Add env variables to run webpack proxy much smoother

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,13 +1,35 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
+
+const insightsProxy = {
+  https: false,
+  ...(process.env.BETA && { deployment: 'beta/apps' }),
+};
+
+const webpackProxy = {
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  env: `${process.env.ENVIRONMENT || 'ci'}-${
+    process.env.BETA ? 'beta' : 'stable'
+  }`, // for accessing prod-beta start your app with ENVIRONMENT=prod and BETA=true
+  appUrl: process.env.BETA
+    ? '/beta/settings/applications'
+    : '/settings/applications',
+  routes: {
+    // insert your API endpoints here if needed
+    ...(process.env.CONFIG_PORT && {
+      [`${process.env.BETA ? '/beta' : ''}/config`]: {
+        host: `http://localhost:${process.env.CONFIG_PORT}`,
+      },
+    }),
+  },
+};
+
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
   useFileHash: false,
-  deployment: process.env.BETA ? 'beta/apps' : 'apps',
-  useProxy: true,
-  appUrl: `/beta/settings/applications`,
-  proxyVerbose: true,
+  ...(process.env.PROXY ? webpackProxy : insightsProxy),
 });
 
 const modulesConfig = require('@redhat-cloud-services/frontend-components-config/federated-modules')(
@@ -19,6 +41,8 @@ const modulesConfig = require('@redhat-cloud-services/frontend-components-config
     },
   }
 );
+
+webpackConfig.devServer.client.overlay = false;
 
 plugins.push(modulesConfig);
 
